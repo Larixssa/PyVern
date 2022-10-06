e43e1b6ec529c80197769cab713d925035746a21
Vertic <thisistherealequinoctic@gmail.com>
2022-10-06 22:38:46 +0800

    Bugfixes

--- src/modules/gui/user_signup.lua
+++ src/modules/gui/user_signup.lua
@@ -10,34 +10,38 @@ function user_signup.init()
 	local strutils = require("src.modules.qyvern_strutils")
 	local oututils = require("src.modules.qyvern_oututils")
 	local file = require("src.modules.qylib.file")
-	local init = require("src.modules.init.init_client")
 	local console_display = require("src.modules.display.console_displayer")
-	local thread = require("src.modules.qylib.thread")
 	local prompt = require("src.modules.display.prompt")
 	local errorutils = require("src.modules.qyvern_errorutils")
+	local init = require("src.modules.init.init_client")
+	local thread = require("src.modules.qylib.thread")
 	local logfile = "logs/logfile.txt"
 	local read = io.read
 	local username
 	oututils.lnOutStr("\n[ Username Sign Up GUI. ]", true)
 	oututils.writeStr(getSCursor() .. " Enter Username: ")
 	username = read()
-	if not (strutils.checkNil(username) or strutils.checkEmpty(username)) then
+	if not (strutils.checkNilOrEmpty(username)) then
 		if not (strutils.checkStringEquals(username, file.readFile("config/profile.txt"))) then
 			local io_option
 			local uname_str
 			prompt.createPrompt(getSCursor() .. " Include border around username?", "yx", "parenthesis")
 			io_option = read()
-			if (strutils.checkStringEquals(io_option, "y") or strutils.checkStringEquals(io_option, "Y")) then
-				uname_str = "[" .. username .. "]"
-			else
-				uname_str = username
-			end
+			prompt.promptCallback(io_option, "y", "Y",
+				function()
+					uname_str = "[" .. username .. "]"
+				end,
+				function()
+					uname_str = username
+				end)
 			file.writeFileBytes("config/profile.txt", uname_str)
 			oututils.lnOutStr(getSCursor() .. " Created Account: \"" .. username .. ".\"", true)
 			console_display.displayConsoleMessage("Successfully created account: \"" .. username .. ".\"", "wf", logfile)
 		else
 			errorutils.localUserProfileExists(username)
 		end
+	else
+		oututils.lnOutStr("")
 	end
 	oututils.lnOutStr(getSCursor() .. " Returning to menu...", true)
 	console_display.displayConsoleMessage("Returning to main...", "wf", logfile)
