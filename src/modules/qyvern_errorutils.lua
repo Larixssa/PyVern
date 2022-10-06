3973170bcde3f1fd37f3d548a8c3d502a0ffc32b
Vertic <thisistherealequinoctic@gmail.com>
2022-10-06 22:40:14 +0800

    Create new failed setup function

--- src/modules/qyvern_errorutils.lua
+++ src/modules/qyvern_errorutils.lua
@@ -10,6 +10,8 @@ function errorutils.execThrowError(_type, kw)
 			oututils.writeStr("ERROR: NULL / NIL " .. kw .. " value." .. escSeqWrite("\n"))
 		elseif (strutils.checkStringEquals(_type, "missing")) then
 			oututils.writeStr("<Missing value: " .. strutils.upperStr(string.gsub(kw, " ", "_")) .. ">" .. escSeqWrite("\n"))
+		elseif (strutils.checkStringEquals(_type, "bad_argument")) then
+			oututils.writeStr("<Bad arguments to function: " .. string.upper(string.gsub(kw, " ", "_")))
 		end
 	end
 end
@@ -39,6 +41,19 @@ function errorutils.invalidTypeError(var, _type)
 	if not (strutils.checkNil(var) and strutils.checkNil(_type)) then
 		if (strutils.typeCheck(var) and strutils.typeCheck(_type)) then
 			oututils.writeStr("Invalid type - \"" .. _type .. "\", for variable: " .. var)
+		end
+	end
+end
+
+function errorutils.setupFailed(_type, msg_callback)
+	local strutils = require("src.modules.qyvern_strutils")
+	local oututils = require("src.modules.qyvern_oututils")
+	if (strutils.checkNil(_type) and strutils.checkNil(msg_callback)) then
+		if (strutils.checkStringEquals(_type, "config_failed")) then
+			oututils.writeStr("Setup failed, configuration files needed.")
+		end
+		if (strutils.typeCheck(msg_callback, "function")) then
+			msg_callback()
 		end
 	end
 end
