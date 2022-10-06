8337a43cf4a9fe8705db487a4de9ad2110d9b1aa
Vertic <thisistherealequinoctic@gmail.com>
2022-10-06 22:38:00 +0800

    Create new prompt callback function

--- src/modules/display/prompt.lua
+++ src/modules/display/prompt.lua
@@ -25,4 +25,29 @@ function prompt.createPrompt(prompt_msg, prompt_type, prompt_subtype)
 	end
 end
 
+--- Creates a callback for a prompt.
+function prompt.promptCallback(s_opt, e_opt, ce_opt, callback, second_callback)
+	local strutils = require("src.modules.qyvern_strutils")
+	local errorutils = require("src.modules.qyvern_errorutils")
+	local file = require("src.modules.qylib.file")
+	local paths = require("src.modules.cl.paths")
+	local nilcheck = strutils.checkNil
+	local equalstrcheck = strutils.checkStringEquals
+	if not (nilcheck(s_opt) and nilcheck(e_opt) and nilcheck(ce_opt) and nilcheck(callback) and nilcheck(second_callback)) then
+		if (equalstrcheck(s_opt, e_opt) or equalstrcheck(s_opt, ce_opt)) then
+			if (strutils.typeCheck(callback, "function")) then
+				file.writeFile(paths.getLogfilePath(), "Callback completed.")
+				callback()
+			end
+		else
+			if (strutils.typeCheck(second_callback, "function")) then
+				file.writeFile(paths.getLogfilePath(), "Second callback completed.")
+				second_callback()
+			end
+		end
+	else
+		errorutils.execThrowError("bad_argument", "prompt callback")
+	end
+end
+
 return prompt
