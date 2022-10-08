664d40ac01aa6772d42330a6eb172aadf3abfb1b
Vertic <thisistherealequinoctic@gmail.com>
2022-10-08 18:04:12 +0800

    Update display/cursor.txt

--- src/modules/display/cursor.lua
+++ src/modules/display/cursor.lua
@@ -1,6 +1,6 @@
 local cursor = {}
 
---- Sets the client cursor.
+--- Sets the client's cursor.
 function cursor.createCursor(cursorCh)
 	local strutils = require("src.modules.qyvern_strutils")
 	local oututils = require("src.modules.qyvern_oututils")
@@ -11,16 +11,17 @@ function cursor.createCursor(cursorCh)
 	end
 end
 
+--- Gets the selection cursor.
 function cursor.getSelectionCursor(scursor_file)
 	local strutils = require("src.modules.qyvern_strutils")
 	local file = require("src.modules.qylib.file")
 	local _scursor
-	if not (strutils.checkNil(scursor_file)) then
-		if (strutils.typeCheck(scursor_file, "string")) then
+	if (strutils.typeCheck(scursor_file, "string")) then
+		if not (strutils.checkNil(scursor_file)) then
 			_scursor = file.readFile(scursor_file)
+		else
+			_scursor = ">"
 		end
-	else
-		_scursor = ">"
 	end
 	return _scursor
 end
