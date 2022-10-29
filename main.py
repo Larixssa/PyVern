b7769ca2e56e84fa956b78101022605460c0a3cb
Larixssa <larissasvendetta@gmail.com>
2022-10-29 14:24:44 +0800

    Improvements.

---
+++ main.py
@@ -0,0 +1,50 @@
+import argparse
+from os import system, path
+from random import randint
+from termcolor import cprint
+from pyfiglet import figlet_format
+
+
+# -----------------<  Text Utilities  >----------------- #
+
+# Text Colors
+RED = "\033[0;31m"
+ORANGE = "\033[0;38m"
+GREEN = "\033[0;32m"
+CYAN = "\033[0;36m"
+PURPLE = "\033[0;35m"
+LIGHT_PURPLE = "\033[1;35m"
+LIGHT_WHITE = "\033[1;37m"
+DARK_GRAY = "\033[1;30m"
+LIGHT_GRAY = "\033[0;37m"
+
+# Styles
+BOLD = "\033[1m"
+ITALIC = "\033[3m"
+UNDERLINE = "\033[4m"
+CROSSED = "\033[9m"
+END = "\033[0m"
+
+# Message Prefixes
+WARN = f"{ORANGE}WARNING{END}"
+ERROR = f"{RED}ERROR{END}"
+SUCCESS = f"{GREEN}SUCCESS{END}"
+
+# -----------------<  General functions  >----------------- #
+
+def generate_title():
+	title = f"""{PURPLE}{BOLD}
+┌─┐ ┬ ┬┬  ┬┌─┐┬─┐┌┐┌
+│─┼┐└┬┘└┐┌┘├┤ ├┬┘│││
+└─┘└ ┴  └┘ └─┘┴└─┘└┘
+	{END}"""
+	cred = "By - Larixssa (Lar_issa)\nVersion: 0.0.1"
+	print(title + "\n" + cred)
+
+# -----------------<  Main function  >----------------- #
+
+def main():
+	generate_title()
+	
+if __name__ == '__main__':
+	main()
