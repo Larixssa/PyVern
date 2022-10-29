2bee609d331038d8ce38a4e6b8ccc9c440e1a0ba
Larixssa <larissasvendetta@gmail.com>
2022-10-29 15:36:05 +0800

    Add new cursor.

--- main.py
+++ main.py
@@ -30,11 +30,7 @@ WARN = f"{ORANGE}WARNING{END}"
 ERROR = f"{RED}ERROR{END}"
 SUCCESS = f"{GREEN}SUCCESS{END}"
 
-# -----------------<  General functions  >----------------- #
-
-def get_file(file):
-	if not file == "":
-		return file
+# -----------------<  Display functions  >----------------- #
 
 def get_credits():
 	creds_f = open(get_file("client_env/credits.txt"), "r")
@@ -44,14 +40,33 @@ def get_version():
 	version_f = open(get_file("client_env/version.txt"), "r")
 	return version_f.read()
 
+def get_cursor(cursor_file):
+	if not cursor_file == "":
+		cursor_str = open(get_file("config/" + cursor_file + ".txt"), "r")
+		return cursor_str.read() + " "
+
+def create_bar(bar_str, bar_length):
+	if not bar_str == "" and bar_length > 0:
+		return bar_str * bar_length
+
+# -----------------<  General functions  >----------------- #
+
+def get_file(file):
+	if not file == "":
+		return file
+
 def generate_title():
 	title = f"""{PURPLE}{BOLD}
 ┌─┐ ┬ ┬┬  ┬┌─┐┬─┐┌┐┌
 │─┼┐└┬┘└┐┌┘├┤ ├┬┘│││
 └─┘└ ┴  └┘ └─┘┴└─┘└┘
 	{END}"""
-	cred = "By - " + get_credits() + "\nVersion: " + get_version()
-	print(title + "\n" + cred)
+	print(title, end="")
+	print("\n" + create_bar("=", 75))
+	cred = "\nBy - " + get_credits() + " - Version: " + get_version()
+	print(cred)
+	print("\n" + get_cursor("cursor"), end="")
+	prompt = input()
 
 # -----------------<  Main function  >----------------- #
 
