4a4af7998a3027dfffa610ce5dc2135907f928b4
Larixssa <larissasvendetta@gmail.com>
2022-11-01 14:22:11 +0800

    Improve setup script.

--- pipeline/config_setup.py
+++ pipeline/config_setup.py
@@ -25,26 +25,48 @@ def write_to_file(_file, content, mode):
 
 def start_setup():
 	default_config_path = "config"
-	os_exec("clear", "ps")
-	if not path.exists(f"{default_config_path}/"):
+
+	required_files = [
+		"set_compile.txt",
+		"set_get_started.txt",
+		"cursor.txt",
+		"username.txt",
+		"userid.txt"
+	]
+
+	if system() == "Windows":
+		os_exec("clear", "ps")
+	elif system() == "Linux":
+		os_exec("clear", "default")
+
+	if system() == "Windows":
 		os_exec("mkdir config", "ps")
+	elif system() == "Linux":
+		os_exec("mkdir config", "default")
+
+	""" Old Method
+	if system() == "Windows":
+		os_exec("ni config/set_compile.txt", "ps")
+		os_exec("ni config/set_get_started.txt", "ps")
+		os_exec("ni config/cursor.txt", "ps")
+	elif system() == "Linux":
+		os_exec("touch config/set_compile.txt", "default")
+		os_exec("touch config/set_get_started.txt", "default")
+		os_exec("touch config/cursor.txt", "default")
+	"""
+
+	for i in range(0, len(required_files)):
 		if system() == "Windows":
-			os_exec("ni config/set_compile.txt", "ps")
-			os_exec("ni config/set_get_started.txt", "ps")
-			os_exec("ni config/cursor.txt", "ps")
+			os_exec(f"ni {default_config_path}/{required_files[i]}", "ps")
 		elif system() == "Linux":
-			os_exec("touch config/set_compile.txt", "default")
-			os_exec("touch config/set_get_started.txt", "default")
-			os_exec("touch config/cursor.txt", "default")
-		write_to_file(f"{default_config_path}/set_compile.txt", "True", "w")
-		write_to_file(f"{default_config_path}/set_get_started.txt", "True", "w")
-		write_to_file(f"{default_config_path}/cursor.txt", "$", "w")
-		if system() == "Windows":
-			os_exec("clear", "ps")
-		elif system() == "Linux":
-			os_exec("clear", "default")
-		print("Finished setting up.")
-	else:
-		print("Configuration folder is already set up.")
+			os_exec(f"touch {default_config_path}/{required_files[i]}", "default")
+
+	write_to_file(f"{default_config_path}/set_compile.txt", "True", "w")
+	write_to_file(f"{default_config_path}/set_get_started.txt", "True", "w")
+	write_to_file(f"{default_config_path}/cursor.txt", "$", "w")
+	if system() == "Windows":
+		os_exec("clear", "ps")
+	elif system() == "Linux":
+		os_exec("clear", "default")
 
 start_setup()
